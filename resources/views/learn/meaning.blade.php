4c6c7e64b48b2713805f0ec6f307b47b396d298d
aman95 <aman1995k@gmail.com>
2016-10-02 16:44:00 +0530

    disqus plugin change

--- resources/views/learn/meaning.blade.php
+++ resources/views/learn/meaning.blade.php
@@ -48,7 +48,7 @@
 
                 (function() { // DON'T EDIT BELOW THIS LINE
                     var d = document, s = d.createElement('script');
-                    s.src = '//vocabx.disqus.com/embed.js';
+                    s.src = '//gyanmarga.disqus.com/embed.js';
                     s.setAttribute('data-timestamp', +new Date());
                     (d.head || d.body).appendChild(s);
                 })();
